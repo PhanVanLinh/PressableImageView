079ef557ceff2b0a0257037816dace8be3fd6d59
PhanVanLinh <phanvanlinh.94vn@gmail.com>
2017-10-10 09:25:15 +0700

    first commit

---
+++ app/src/main/java/toong/vn/pressableimageview/MainActivity.java
@@ -0,0 +1,22 @@
+package toong.vn.pressableimageview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.imageview).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "click", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
---
+++ app/src/main/java/toong/vn/pressableimageview/PressableImageView.java
@@ -0,0 +1,55 @@
+package toong.vn.pressableimageview;
+
+import android.content.Context;
+import android.support.v7.widget.AppCompatImageView;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+
+public class PressableImageView extends AppCompatImageView {
+    private static final float DEFAULT_ALPHA_WHEN_PRESS = 0.5f;
+    private static final float DEFAULT_ALPHA = 1f;
+
+    public PressableImageView(Context context) {
+        super(context);
+    }
+
+    public PressableImageView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public PressableImageView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    private void refresh() {
+        if (isPressed()) {
+            setAlpha(DEFAULT_ALPHA_WHEN_PRESS);
+            invalidate();
+            return;
+        }
+        setAlpha(DEFAULT_ALPHA);
+        invalidate();
+    }
+
+    @Override
+    public void setPressed(boolean pressed) {
+        super.setPressed(pressed);
+        refresh();
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if (event.getAction() == android.view.MotionEvent.ACTION_DOWN) {
+            setPressed(true);
+        } else if (event.getAction() == android.view.MotionEvent.ACTION_UP) {
+            setPressed(false);
+            float x = event.getX();
+            float y = event.getY();
+            boolean isInside = (x > 0 && x < getWidth() && y > 0 && y < getHeight());
+            if(isInside){
+                performClick();
+            }
+        }
+        return true;
+    }
+}
